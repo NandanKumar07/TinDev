522cd106deece497a62802a6d1f632d109fe6cd1
NandanKumar07 <onlinestudy641@gmail.com>
2025-03-10 13:11:25 +0530

    Backend for Tindec

--- BackEnd/src/config/database.js
+++ BackEnd/src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const DBconnect = async () => {
   await mongoose.connect(
-    "mongodb+srv://onlinestudy641:6eeDOWBA354A320u@cluster0.9kspm.mongodb.net/tindev"
+    "xyz"
   );
 };
 
